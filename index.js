3b23879392fac841e7733d931fb132ebc4caa375
abdurrazzakhera <hhera1413@gmail.com>
2022-06-11 16:33:10 +0600

    'dd'

--- index.js
+++ index.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 5000;
 
 //middleware
 app.use(cors());
+app.use(express.json());
 
 const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.eyaye.mongodb.net/?retryWrites=true&w=majority`;
